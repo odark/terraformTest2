a52f121040f858cb93760157992bfc8082a377b9
오승현 <odark74@gmail.com>
2021-09-10 00:01:39 +0900

    test2

--- main.tf
+++ main.tf
@@ -2,12 +2,3 @@ provider "aws" {
     region = "ap-northeast-2"
 }
 
-data "terraform_remote_state" "test_instance" {
-    backend = "remote"
-    config {
-        organization = "odarkTeraTest"
-        workspaces {
-            name = "terraformTest"
-        }
-    }
-}
