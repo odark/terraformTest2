c88740c55d1f40462e05e9dfc93fb7b92bb0648f
오승현 <odark74@gmail.com>
2021-09-13 10:55:16 +0900

    test var

--- main.tf
+++ main.tf
@@ -11,3 +11,13 @@ data "terraform_remote_state" "test_instance" {
         }
     }
 }
+
+
+resource "aws_instance" "web1" {
+  ami           = "ami-0ba5cd124d7a79612"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "${data.terraform_remote_state.test_instance.ec2_id}"-1
+  }
+}
--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,4 @@
-output "test" {
-    value = "${data.terraform_remote_state.test_instance.ec2_name}"
+output "test1" {
+    value = "${aws_instance.web1.ami}"#data.terraform_remote_state.test_instance.ec2_name
+
 }
