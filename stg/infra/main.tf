e50e1635afa6445a4d6f72a9c4f758e59b5d377f
seunghyun-oh <seunghyun08.oh@partner.sec.co.kr>
2021-12-03 14:15:28 +0900

    instance추가

--- stg/infra/main.tf
+++ stg/infra/main.tf
@@ -1,5 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
 provider "aws" {
-    region = var.region
+#    access_key = "AKIA24HTW6KSTLQV4BTQ"
+#    secret_key = "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"
+    region = "ap-northeast-2"
+    
 }
 
 data "terraform_remote_state" "stg_test_instance" {
@@ -12,7 +26,7 @@ data "terraform_remote_state" "stg_test_instance" {
     }
 }
 
-resouce "aws_instance" "app_server" {
+resource "aws_instance" "app_server" {
     ami = "ami-0263588f2531a56bd"
     instance_type="t2.micro"
 
